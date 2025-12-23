347ffa111f160907bb8f057246ee09e6f044c524
rwygmans <68482511+rwygmans@users.noreply.github.com>
2025-12-22 19:07:45 -0500

    Add type declarations for deck.gl and react-map-gl

---
+++ app/types.d.ts
@@ -0,0 +1,6 @@
+// Type declarations for modules without TypeScript definitions
+declare module '@deck.gl/react';
+declare module '@deck.gl/core';
+declare module '@deck.gl/layers';
+declare module 'react-map-gl/mapbox';
+
